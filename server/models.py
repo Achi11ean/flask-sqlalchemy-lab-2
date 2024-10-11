53723bc9b57e4bd7caafb9782df0ac2d9f359421
Jonathen Whitford <paredes.jonathen@yahoo.com>
2024-10-11 03:49:01 -0400

    completed lab

--- server/models.py
+++ server/models.py
@@ -10,23 +10,57 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+# Review Model
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=True)
 
-class Customer(db.Model):
+    customer = db.relationship('Customer', backref='reviews')
+    item = db.relationship('Item', backref='reviews')
+
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    def __init__(self, comment='', customer=None, item=None):
+        self.comment = comment
+        self.customer = customer
+        self.item = item
+
+# Customer Model with Association Proxy
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    items = association_proxy('reviews', 'item')
+    serialize_rules = ('-reviews',)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            # Handle cases where item might be None
+            'items': [item.name for item in self.items if item],  # List of item names, skip None
+            'reviews': [review.comment for review in self.reviews]
+        }
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+# Item Model
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
-
+    serialize_rules = ('-reviews',)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'reviews': [review.comment for review in self.reviews]
+        }
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
